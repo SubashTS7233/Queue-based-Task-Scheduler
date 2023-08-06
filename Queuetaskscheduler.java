1dc0857c92d818caf67f394532e1e458d1bfab0f
SubashTS7233 <138566800+SubashTS7233@users.noreply.github.com>
2023-08-06 14:34:38 +0530

    Update Queuetaskscheduler.java

--- Queuetaskscheduler.java
+++ Queuetaskscheduler.java
@@ -8,40 +8,30 @@ class Task implements Comparable<Task> {
         this.name = name;
         this.priority = priority;
     }
-
     public String getName() {
         return name;
     }
-
     public int getPriority() {
         return priority;
     }
-
-    @Override
     public int compareTo(Task other) {
-        // Higher priority tasks should come before lower priority tasks in the queue
         return Integer.compare(other.priority, this.priority);
     }
 }
-
 class TaskScheduler {
     private PriorityQueue<Task> taskQueue;
-
     public TaskScheduler() {
         taskQueue = new PriorityQueue<>();
     }
-
     public void addTask(String name, int priority) {
         Task newTask = new Task(name, priority);
         taskQueue.add(newTask);
     }
-
     public void removeTask() {
         if (!taskQueue.isEmpty()) {
             taskQueue.poll();
         }
     }
-
     public void executeNextTask() {
         if (!taskQueue.isEmpty()) {
             Task nextTask = taskQueue.poll();
@@ -51,31 +41,24 @@ class TaskScheduler {
         }
     }
 }
-
  class Queuetaskscheduler {
     public static void main(String[] args) {
         TaskScheduler scheduler = new TaskScheduler();
-
-        // Adding tasks with different priorities
         scheduler.addTask("Task 1", 3);
         scheduler.addTask("Task 2", 1);
         scheduler.addTask("Task 3", 2);
 
-        // Executing tasks based on priority
-        scheduler.executeNextTask(); // Task 2 with priority 1
-        scheduler.executeNextTask(); // Task 3 with priority 2
-        scheduler.executeNextTask(); // Task 1 with priority 3
+        scheduler.executeNextTask(); 
+        scheduler.executeNextTask();
+        scheduler.executeNextTask(); 
 
-        // Adding more tasks
         scheduler.addTask("Task 4", 1);
         scheduler.addTask("Task 5", 4);
 
-        // Removing tasks
         scheduler.removeTask();
 
-        // Executing tasks based on updated priority queue
-        scheduler.executeNextTask(); // Task 4 with priority 1
-        scheduler.executeNextTask(); // Task 5 with priority 4
-        scheduler.executeNextTask(); // No tasks to execute.
+        scheduler.executeNextTask(); 
+        scheduler.executeNextTask(); 
+        scheduler.executeNextTask(); 
     }
 }
